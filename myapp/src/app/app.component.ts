252dafc7bd3c74baa5d82d02daa557d93bcc1756
Azza Rojbi <arojbi@my.bcit.ca>
2017-11-03 20:18:46 -0700

    playing around :)

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
+export class PlayingCard {
+    cardVal: string;
+    suit:    string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Cool World';
+  name = 'Azza';
 }
