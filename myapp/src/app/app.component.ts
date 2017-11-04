e949bb08ff5623525fff550bf20e1f0fc57d875e
Azza Rojbi <arojbi@my.bcit.ca>
2017-11-03 20:43:13 -0700

    Two Way Data Binding

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -2,19 +2,21 @@ import { Component } from '@angular/core';
 
 export class PlayingCard {
     cardVal: string;
-    suit:    string;
+    suit: string;
 }
+
 @Component({
     selector: 'app-root',
-    // Multi-line content is allowed with back ticks.
+    // Multi-line content allowed with back ticks.
     template: `<h1>Hello world!  {{title}} <br/>
-	       {{card.cardVal}} of {{card.suit}}.</h1>`
+            {{card.cardVal}} of {{card.suit}}.</h1>
+               Card: <input [(ngModel)]="card.cardVal">`
 })
 
 export class AppComponent {
-    public title = 'This is Angular!';
+    public title = 'This is Angular 4!';
 
-    // Declare and initialize a PlayingCard object.
+    // Declare a PlayingCard object.
     public card: PlayingCard = {
         cardVal: "Ace",
         suit: "Spades"
