65dd421049038abdffb62992432b411dd2a93977
Azza Rojbi <arojbi@my.bcit.ca>
2017-11-03 20:22:21 -0700

    export class exemple

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -4,13 +4,19 @@ export class PlayingCard {
     cardVal: string;
     suit:    string;
 }
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    // Multi-line content is allowed with back ticks.
+    template: `<h1>Hello world!  {{title}} <br/>
+	       {{card.cardVal}} of {{card.suit}}.</h1>`
 })
+
 export class AppComponent {
-  title = 'Cool World';
-  name = 'Azza';
+    public title = 'This is Angular!';
+
+    // Declare and initialize a PlayingCard object.
+    public card: PlayingCard = {
+        cardVal: "Ace",
+        suit: "Spades"
+    };
 }
